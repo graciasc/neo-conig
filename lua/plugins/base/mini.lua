a3ae74306345b564731f5f7a76862cace2acb06d
gracias <gracias.claude@outlook.com>
2023-12-14 19:54:01 -0700

    stable
    - add mini keymaps that I'm familiar withk
    - add back dashboard.nvim with a custom startup

---
+++ lua/plugins/base/mini.lua
@@ -0,0 +1,14 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "msa",
+      delete = "msd",
+      find = "msf",
+      find_left = "msF",
+      highlight = "msh",
+      replace = "msr",
+      update_n_lines = "msn",
+    },
+  },
+}
